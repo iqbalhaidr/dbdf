ae277cf051f8b6f40393a6f5793709579d380066
Iqbal Haidar <haidariqbalm@gmail.com>
2026-07-01 22:44:31 +0700

    feat: pandas df support for write_database()

--- src/dbdf/core.py
+++ src/dbdf/core.py
@@ -1,15 +1,20 @@
 import polars as pl
+import pandas as pd
 from urllib.parse import urlparse
 
 from . import postgres
 
 def write_database(
     uri: str,
-    df: pl.DataFrame,
+    df: pl.DataFrame | pd.DataFrame,
     table_name: str,
     mode: str = "append",
     identifier: str = None
 ):
+    # Convert to polars if df is pandas
+    if isinstance(df, pd.DataFrame):
+        df = pl.from_pandas(df, include_index=True)
+
     scheme = urlparse(uri).scheme
     match scheme:
         case "postgresql":
