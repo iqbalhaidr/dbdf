a0bc4b8e974faa93da84466c7125e7d3d989a025
Iqbal Haidar <haidariqbalm@gmail.com>
2026-07-02 10:47:56 +0700

    feat: ddl

--- src/dbdf/core.py
+++ src/dbdf/core.py
@@ -9,15 +9,16 @@ def write_database(
     df: pl.DataFrame | pd.DataFrame,
     table_name: str,
     mode: str = "append",
-    identifier: str = None
+    identifier: str = None,
+    dtype_overrides: dict[type, str] = None
 ):
     # Convert to polars if df is pandas
     if isinstance(df, pd.DataFrame):
-        df = pl.from_pandas(df, include_index=True)
+        df = pl.from_pandas(df, include_index=False)
 
     scheme = urlparse(uri).scheme
     match scheme:
         case "postgresql":
-            postgres.write_database(uri, df, table_name, mode, identifier)
+            postgres.write_database(uri, df, table_name, mode, identifier, dtype_overrides)
         case "oracle":
             return
--- src/dbdf/postgres.py
+++ src/dbdf/postgres.py
@@ -1,10 +1,14 @@
 import adbc_driver_postgresql.dbapi
+import polars as pl
+
 from .ddl import generate_ddl
 
 # TODO: kasus table belum ada di database (table baru)
 # TODO: chunking
-def write_database(uri, df, table_name, mode, identifier):
+def write_database(uri, df, table_name, mode, identifier, dtype_overrides):
     with adbc_driver_postgresql.dbapi.connect(uri) as conn:
+        _ensure_table_exists(conn, df, table_name, identifier, dtype_overrides)
+
         match mode:
             case "append":
                 _append(conn, df, table_name)
@@ -69,28 +73,74 @@ def _upsert(conn, df, table_name, identifier):
             {action};
         """
         cur.execute(QUERY_UPSERT)
+    
+# Polars to PostgreSQL Type Mapping
+POLARS_TO_POSTGRES: dict[type, str] = {
+    pl.Int8: "SMALLINT",
+    pl.Int16: "SMALLINT",
+    pl.Int32: "INTEGER",
+    pl.Int64: "BIGINT",
+    pl.Int128: "NUMERIC(39)",
+    pl.UInt8: "SMALLINT",
+    pl.UInt16: "INTEGER",
+    pl.UInt32: "BIGINT",
+    pl.UInt64: "NUMERIC(20)",
+    pl.Float16: "REAL",
+    pl.Float32: "REAL",
+    pl.Float64: "DOUBLE PRECISION",
+    pl.Date: "DATE",
+    pl.Time: "TIME",
+    pl.Datetime: "TIMESTAMP",
+    pl.Duration: "INTERVAL",
+    pl.String: "VARCHAR",
+    pl.Categorical: "VARCHAR",
+    pl.Utf8: "VARCHAR",
+    pl.Binary: "BYTEA",
+    pl.Boolean: "BOOLEAN",
+    pl.Null: "VARCHAR",
+}
 
-def _table_exists(conn, table_name) -> bool:
+def _infer_dtype(dtype: pl.DataType):
+    datatype = POLARS_TO_POSTGRES.get(dtype)
+    return datatype if datatype is not None else "VARCHAR"
+
+def _is_table_exists(conn, table_name) -> bool:
     with conn.cursor() as cur:
-        cur.execute(
-            "SELECT 1 FROM information_schema.tables WHERE table_name = %s",
-            [table_name],
-        )
+        cur.execute(f"""
+            SELECT 1 
+            FROM information_schema.tables 
+            WHERE table_name = '{table_name}' 
+            AND table_schema = current_schema();
+        """)
         return cur.fetchone() is not None
 
-def _ensure_table(conn, df, table_name, identifier):
-    if _table_exists(conn, table_name):
+def _ensure_table_exists(conn, df, table_name, identifier, dtype_overrides):
+    if _is_table_exists(conn, table_name):
         return
-    if isinstance(identifier, str):
-        identifier = [identifier]
-    ddl = generate_ddl(
-        dataframe=df,
-        table_name=table_name,
-        dialect="postgresql",
-        primary_key=identifier or None,
-    )
+    
+    print(f"[DBDF] Table '{table_name}' not found. Auto-creating...")
+
+    # Infer datatype
+    dtype_overrides = dtype_overrides or {}
+    cols_def = []
+    for col_name, dtype in df.schema.items():
+        pg_type = dtype_overrides.get(col_name) or _infer_dtype(dtype)
+        cols_def.append(f'"{col_name}" {pg_type}')
+    cols_sql = ", ".join(cols_def)
+
+    # Infer primary key
+    pk_sql = ""
+    if identifier:
+        if isinstance(identifier, str):
+            identifier = [identifier]
+        pk_cols = ", ".join(f'"{c}"' for c in identifier)
+        pk_sql = f", PRIMARY KEY ({pk_cols})"
+
+    # Execute ddl
+    QUERY_CREATE = f'CREATE TABLE "{table_name}" ({cols_sql}{pk_sql});'
+    print(f"[DBDF] Executing: {QUERY_CREATE}")
     with conn.cursor() as cur:
-        cur.execute(ddl)
+        cur.execute(QUERY_CREATE)
 
 # Benchmark
 '''
